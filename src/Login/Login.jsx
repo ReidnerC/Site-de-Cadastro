f5e0242d975b8fb62a975b72b430e2b6408bc2a9
unknown <reidner.cairo@gmail.com>
2024-12-16 23:32:02 -0300

    segundo commit: interface site cadastro

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -5,25 +5,34 @@ import { useState } from "react";
 import "./Login.css";
 
 const Login = () => {
-    const [usernome, setUsername] = useState("");
+    const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const handleSubmit = (event) => {
-        event.preventDefault();
+      event.preventDefault(); 
         
-        console.log("Envio");
+    alert("Enviando os dados:" + username + " - " + password);
     };
 
   return (
     <div className="container">
         <form onSubmit={handleSubmit}>
       <h1>Acesse o sistema</h1>
-      <div>
-      <input type="email" placeholder="E-mail" />
+      <div className="input-field">
+      <input
+        type="email"
+        placeholder="E-mail" 
+        required
+        onChange= {(e) => setUsername(e.target.value)}
+        /> 
       <FaUser className="icon" />
       </div>
-      <div>
-      <input type="password" placeholder="Senha" />
+      <div className="input-field">
+      <input 
+        type="password" 
+        placeholder="Senha"
+        onChange={(e) => setPassword(e.target.value)} 
+        />
       <FaLock className="icon" />
       </div>
 
